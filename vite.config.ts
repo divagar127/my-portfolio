3d6db4c4c701194dd0aa92ed3437d56ac67b0128
divagar127 <div27122005@gmail.com>
2026-05-12 16:26:55 +0530

    fix: use tanstackStart plugin to resolve index.html build error

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    tsconfigPaths(),
+    tanstackStart(),
     react(),
     tailwindcss(),
-    tsconfigPaths(),
   ],
   resolve: {
     alias: {
